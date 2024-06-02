03e1b28bbfc20ecb76ee04e3cde5ce500dc59d89
Appmedia06 <joey5489joey@gmail.com>
2024-06-02 21:32:52 +0800

    i_sqrt version2

--- Test1/i_sqrt.c
+++ Test1/i_sqrt.c
@@ -1,5 +1,5 @@
 #include <math.h>
-int i_sqrt(int N)
+int i_sqrt1(int N)
 {
     int msb = (int) log2(N);
     int a = 1 << msb;
@@ -12,11 +12,30 @@ int i_sqrt(int N)
     return result;
 }
 
+int i_sqrt2(int N)
+{
+    int msb = 0;
+    int n = N;
+    while (n > 1) {
+        n >>= 1;
+        msb++;
+    }
+    int a = 1 << msb;
+    int result = 0;
+    while (a != 0) {
+        if ((result + a) * (result + a) <= N)
+            result += a;
+        a >>= 1;
+    }
+    return result;
+}
+
+
 #include <stdio.h>
 int main()
 {
     int N = 36;
-    printf("%d\n", i_sqrt(N));
+    printf("%d\n", i_sqrt2(N));
     return 0;
 }
 
