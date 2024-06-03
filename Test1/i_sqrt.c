503d32f0cda8fac8797737086e958108e262f373
Appmedia06 <joey5489joey@gmail.com>
2024-06-03 22:28:43 +0800

    Replace the __builtin_clz with fls

--- Test1/i_sqrt.c
+++ Test1/i_sqrt.c
@@ -1,6 +1,17 @@
 #include <math.h>
 #include <stdio.h>
 
+int fls(int word) {
+    int r = 32;
+    if (!word) return 0;
+    if (!(word & 0xFFFF0000u)) { word <<= 16; r -= 16; }
+    if (!(word & 0xFF000000u)) { word <<= 8; r -= 8; }
+    if (!(word & 0xF0000000u)) { word <<= 4; r -= 4; }
+    if (!(word & 0xC0000000u)) { word <<= 2; r -= 2; }
+    if (!(word & 0x80000000u)) { r -= 1; }
+    return r;
+}
+
 int i_sqrt1(int N)
 {
     int msb = (int) log2(N);
@@ -38,7 +49,7 @@ int i_sqrt3(int x)
         return x;
 
     int z = 0;
-    for (int m = 1UL << ((31 - __builtin_clz(x)) & ~1UL); m; m >>= 2) {
+    for (int m = 1UL << ((fls(x) - 1) & ~1UL); m; m >>= 2) {
         int b = z + m;
         z >>= 1;
         if (x >= b)
