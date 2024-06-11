4101e4a6153fe8f9dc70d044f31cbbf60d634909
Appmedia06 <joey5489joey@gmail.com>
2024-06-11 16:46:44 +0800

    implement ceil_ilog2()

---
+++ Test5/ceil_ilog2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int ceil_ilog2(uint32_t x)
+{
+    uint32_t r, shift;
+
+    x--;
+    r = (x > 0xFFFF) << 4;
+    x >>= r;
+    shift = (x > 0xFF) << 3;
+    x >>= shift;
+    r |= shift;
+    shift = (x > 0xF) << 2;
+    x >>= shift;
+    r |= shift;
+    shift = (x > 0x3) << 1;
+    x >>= shift;
+    return (r | shift | x > 1) + 1; // GGGG = 1
+}
+
+int main(void)
+{
+    uint32_t x = 18;
+    printf("ceil_ilog(%d) = %d\n", x, ceil_ilog2(x));
+    return 0;
+}
